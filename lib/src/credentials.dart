cd39bc2135e1af90da8e7d493da03e53ba3b0daf
kumar8600 <kumar1@hotmail.co.jp>
2014-06-06 02:14:54 +0900

    Add JSON convertion for Credentials

--- lib/src/credentials.dart
+++ lib/src/credentials.dart
@@ -1,7 +1,9 @@
 library credentials;
 
+import 'dart:convert';
+
 /**
- * A class describing OAuth credentials except for client credentials.
+ * A class describing OAuth credentials except for client credential
  */
 class Credentials {
   final String _token;
@@ -17,11 +19,21 @@ class Credentials {
     }
     return new Credentials(parameters['oauth_token'], parameters['oauth_token_secret']);
   }
+  factory Credentials.fromJSON(String json) {
+    return new Credentials.fromMap(JSON.decode(json));
+  }
 
   String get token => _token;
   String get tokenSecret => _tokenSecret;
 
   String toString() {
     return 'oauth_token=$token&oauth_token_secret=$tokenSecret';
+  }
+
+  Map toJSON() {
+    return {
+      "oauth_token": token,
+      "oauth_token_secret": tokenSecret
+    };
   }
 }
