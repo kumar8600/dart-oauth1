da95ad5ab3a9ffa3cc12efb04397a392753688d8
kumar8600 <kumar8600@gmail.com>
2014-12-14 06:26:06 +0900

    Remove platforms utility definitions including twitter

--- test/authorization_test.dart
+++ test/authorization_test.dart
@@ -5,12 +5,17 @@ import 'package:http/http.dart' as http;
 import 'package:oauth1/oauth1.dart';
 
 void main() {
+  Platform twitter1_1 = new Platform(
+        'https://api.twitter.com/oauth/request_token',
+        'https://api.twitter.com/oauth/authorize',
+        'https://api.twitter.com/oauth/access_token',
+        SignatureMethods.HMAC_SHA1);
   const String apiKey = 'LLDeVY0ySvjoOVmJ2XgBItvTV';
   const String apiSecret = 'JmEpkWXXmY7BYoQor5AyR84BD2BiN47GIBUPXn3bopZqodJ0MV';
   ClientCredentials clientCredentials = new ClientCredentials(apiKey, apiSecret);
 
   test('request temporary credentials', () {
-    Authorization auth = new Authorization(clientCredentials, Platforms.TWITTER1_1, new http.Client());
+    Authorization auth = new Authorization(clientCredentials, twitter1_1, new http.Client());
     return auth.requestTemporaryCredentials();
   });
 }
